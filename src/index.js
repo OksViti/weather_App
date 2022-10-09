b2c722e0829aa1847a35d90de8bec4e2163f5e1b
OksViti <114354593+OksViti@users.noreply.github.com>
2022-10-09 23:40:46 +0300

    add forecast

--- src/index.js
+++ src/index.js
@@ -18,6 +18,71 @@ function currentTime() {
       minutes;
     return currentTime;
   }
+
+  function formatDay(timestamp) {
+    let date = new Date(timestamp*1000);
+    let day = date.getDay();
+    let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+    return days[day];
+
+  };
+
+  function formatHour(timestamp) {
+    let date = new Date(timestamp);
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    timeHour = hour+" : "+min;
+    return timeHour;
+  };
+
+  function displayForecast(response) {
+    console.log(response.data.hourly);
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function(forecastDay,index){
+      if (index < 6){
+      forecastHTML = forecastHTML+`
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+          <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" class="icons">
+            <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span> <br />
+              <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
+            </div>
+          </div>
+        `;
+      }
+    })
+    forecastHTML = forecastHTML+"</div>";
+    forecastElement.innerHTML = forecastHTML;
+
+
+    let forecastHour = response.data.hourly;
+    let forecastHourElement = document.querySelector("#hourly-forecast");
+    let forecastHourHTML = `<div class="row">`;
+
+    forecast.forEach(function(forecastHour,index){
+    if (index < 6){
+    forecastHourHTML = forecastHourHTML+`
+      <div class="col-2">
+        05:00
+        <strong>${Math.round(forecastHour.temp.min)}°</strong>
+      </div>
+    `;
+    }
+  })
+  forecastHourHTML = forecastHourHTML+"</div>";
+  forecastHourElement.innerHTML = forecastHourHTML;
+  }
+
+  function getForecast(coordinates){
+   
+    let apiKey = "62231151ce343c4d68652e1617efc22f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+  }
+
   
   let dateForm = document.querySelector("#current-date");
   dateForm.innerHTML = currentTime();
@@ -65,7 +130,10 @@ function currentTime() {
     );
     document.querySelector("#description").innerHTML =
       response.data.weather[0].main;
-  }
+
+    getForecast(response.data.coord);
+   }
+
   function searchCity(city) {
     let apiKey = "62231151ce343c4d68652e1617efc22f";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -80,8 +148,7 @@ function currentTime() {
   
   function searchLocation(position) {
     let apiKey = "62231151ce343c4d68652e1617efc22f";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&exclude={}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeatherCondition);
   }
   
@@ -89,6 +156,8 @@ function currentTime() {
     event.preventDefault();
     navigator.geolocation.getCurrentPosition(searchLocation);
   }
+
+
   
   let celsiusTemperature = null;
 
@@ -99,4 +168,5 @@ function currentTime() {
   currentLocationButton.addEventListener("click", getCurrentLocation);
   
   searchCity("Kyiv");
+  //displayForecast();
   
